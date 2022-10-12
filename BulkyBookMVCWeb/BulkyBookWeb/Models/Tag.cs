cd71f47da4adcb9adb1df508e9f06c12d2527d58
Karolina Sniezek <karolina.sniezek@oneadvanced.com>
2022-10-12 22:04:42 +0200

    add using and productId to tag model

--- BulkyBookMVCWeb/BulkyBookWeb/Models/Tag.cs
+++ BulkyBookMVCWeb/BulkyBookWeb/Models/Tag.cs
@@ -9,6 +9,7 @@ namespace BulkyBookWeb.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
     }
 }
